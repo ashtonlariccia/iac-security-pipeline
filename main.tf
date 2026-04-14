57b1a185d15023d10e4bdc405560869b6a3acb94
ashtonlariccia <ashtonlariccia267@gmail.com>
2026-04-14 15:39:08 -0400

    added encryption, bucket policy, & logging

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
@@ -14,7 +14,7 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check = true
   skip_requesting_account_id = true
-  s3_use_path_style  = true
+  s3_use_path_style = true
 
   endpoints {
     s3 = "http://localhost:4566"
@@ -25,4 +25,45 @@ provider "aws" {
 
 resource "aws_s3_bucket" "demo" {
   bucket = "my-demo-bucket"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "DenyUnauthorizedAccess"
+        Effect = "Deny"
+        Principal = "*"
+        Action = "s3:*"
+        Resource = [
+          aws_s3_bucket.demo.arn,
+          "${aws_s3_bucket.demo.arn}/*",
+        ]
+        Condition = {
+          IpAddress = {
+            "aws:SourceIp" = "0.0.0.0/0"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_logging" "demo" {
+  bucket = aws_s3_bucket.demo.id
+  target_bucket = aws_s3_bucket.demo.id
+  target_prefix = "/logs/"
 }
